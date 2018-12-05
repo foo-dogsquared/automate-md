73e3eb3c3c97b3feb2df381ce654473e6f9d3466
foo-dogsquared <christiangabrielarazas@gmail.com>
2018-12-05 14:06:04 +0800

    Complete the 'extract_frontmatter' function

--- src/functions/file_io.hpp
+++ src/functions/file_io.hpp
@@ -6,6 +6,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <regex>
 
 frontmatter extract_frontmatter(std::string __file_path) {
 	frontmatter _output;
@@ -20,22 +21,25 @@ frontmatter extract_frontmatter(std::string __file_path) {
 	bool _is_opening_tag_parse = false, _is_closing_tag_parse = false;
 
 	std::string _line;
-	std::regex _key_value_regex("^\\s*\"?(\\w+)\"?" + _fm_type_obj.__assigner + "\\s*(.+)");
+	std::regex _key_value_regex;
+	std::smatch _matches;
 	while (!_is_opening_tag_parse || !_is_closing_tag_parse) {
 		getline(_input_file, _line);
 
 		if (_line == "+++" || _line == "---" || _line == "{" || _line == "}") {
 			_fm_type_obj.__type = detect_type(_line);
 			_fm_type_obj = init(_fm_type_obj.__type);
+			_key_value_regex = "^\\s*\"?(\\w+)\"?" + _fm_type_obj.__assigner + "\\s*(.+)";
 
 			if (!_is_opening_tag_parse)
 				_is_opening_tag_parse = true;
-			else if (!_is_closing_tag_parse)
+			else if (!_is_closing_tag_parse) {
 				_is_closing_tag_parse = true;
+				_input_file.close();
+				break;
+			}
 		}
-
-		std::smatch _matches;
-		if (_is_opening_tag_parse && std::regex_match(_line, _matches, _key_value_regex)) {
+		else if (_is_opening_tag_parse && std::regex_match(_line, _matches, _key_value_regex)) {
 			std::ssub_match _key = _matches[1];
 			std::ssub_match _value = _matches[2];
 			std::cout << _matches[2]  << std::endl;
@@ -51,7 +55,9 @@ frontmatter extract_frontmatter(std::string __file_path) {
 				_output.categories = arr_extract(_value);
 			else if (removeQuote(_key) == "tags")
 				_output.tags = arr_extract(_value);
-		}			
+			else
+				_output.others.insert(std::make_pair(_key.str(), _value.str()));
+		}
 	}
 
 	return _output;
@@ -69,15 +75,15 @@ int post_write(std::string __file_path, frontmatter __frontmatter, std::string _
 
 	// Writing the frontmatter in the file
 	__output_file << __f.__open_divider << std::endl;
-	__output_file << __f.__tab << is_json("layout", __frontmatter_type) << __f.__assigner << " " << encloseQuote(__frontmatter.layout) << std::endl;
-	__output_file << __f.__tab << is_json("title", __frontmatter_type) << __f.__assigner << " " << encloseQuote(__frontmatter.title) << std::endl;
-	__output_file << __f.__tab << is_json("date", __frontmatter_type) << __f.__assigner << " " << encloseQuote(__frontmatter.date) << std::endl;
-	__output_file << __f.__tab << is_json("author", __frontmatter_type) << __f.__assigner << " " << encloseQuote(__frontmatter.author) << std::endl;
+	__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("layout"), "layout") << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(__frontmatter.layout), __frontmatter.layout) << is_json(__frontmatter_type, ",", "") << std::endl;
+	__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("title"), "title") << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(__frontmatter.title), __frontmatter.title) << is_json(__frontmatter_type, ",", "") << std::endl;
+	__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("date"), "date") << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(__frontmatter.date), __frontmatter.date) << is_json(__frontmatter_type, ",", "") << std::endl;
+	__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("author"), "author") << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(__frontmatter.author), __frontmatter.author) << is_json(__frontmatter_type, ",", "") << std::endl;
 
 	if (__frontmatter.categories_length > 0)
-		__output_file << __f.__tab << is_json("categories", __frontmatter_type) << __f.__assigner << " " << encloseQuote_arr(__frontmatter.categories, __frontmatter.categories_length) << std::endl;
+		__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("categories"), "categories") << __f.__assigner << " " << encloseQuote_arr(__frontmatter.categories, __frontmatter.categories_length) << is_json(__frontmatter_type, ",", "") << std::endl;
 	if (__frontmatter.tags_length > 0)
-		__output_file << __f.__tab << is_json("tags", __frontmatter_type) << __f.__assigner << " " << encloseQuote_arr(__frontmatter.tags, __frontmatter.tags_length) << std::endl;
+		__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("tags"), "tags") << __f.__assigner << " " << encloseQuote_arr(__frontmatter.tags, __frontmatter.tags_length) << std::endl;
 
 	__output_file << __f.__close_divider << std::endl;
 
@@ -88,13 +94,13 @@ int post_write(std::string __file_path, frontmatter __frontmatter, std::string _
 }
 
 int post_parse(std::string __file_path) {
-
+	return 0;
 }
 
 int post_extract(std::string __file_path, std::string __part = "frontmatter") {
 	frontmatter _fm = extract_frontmatter(__file_path);
 
-	std::cout << _fm.categories[0] << std::endl;
+	std::cout << _fm.date << std::endl;
 
 	return 0;
 }
