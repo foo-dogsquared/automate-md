e2d236e69336e050097d42c715ec529bc6c6c468
foo-dogsquared <christiangabrielarazas@gmail.com>
2018-12-06 11:04:46 +0800

    Refactor according to the new frontmatter struct

--- src/functions/file_io.hpp
+++ src/functions/file_io.hpp
@@ -8,14 +8,31 @@
 #include <iostream>
 #include <regex>
 
+#define FILE_IO_ERROR_NUM 3
+
+int open_file_i(std::ifstream &__input_file, std::string __input_path) {
+	__input_file.open(__input_path);
+
+	if (!__input_file.is_open())
+		return return_error_code(3, "Cannot open file at: " + __input_path);
+
+	return 0;
+}
+
+int open_file_o(std::ofstream &__output_file, std::string __output_path) {
+	__output_file.open(__output_path);
+
+	if (!__output_file.is_open())
+		return return_error_code(3, "Cannot open file at: " + __output_path);
+
+	return 0;
+}
+
 frontmatter extract_frontmatter(std::string __file_path) {
 	frontmatter _output;
 	frontmatter_type _fm_type_obj;
 	std::ifstream _input_file;
-
-	_input_file.open(__file_path);
-
-	if (!_input_file.is_open())
+	if (open_file_i(_input_file, __file_path) == FILE_IO_ERROR_NUM)
 		return _output;
 
 	bool _is_opening_tag_parse = false, _is_closing_tag_parse = false;
@@ -23,6 +40,7 @@ frontmatter extract_frontmatter(std::string __file_path) {
 	std::string _line;
 	std::regex _key_value_regex;
 	std::smatch _matches;
+	std::ssub_match _key, _value;
 	while (!_is_opening_tag_parse || !_is_closing_tag_parse) {
 		getline(_input_file, _line);
 
@@ -40,23 +58,9 @@ frontmatter extract_frontmatter(std::string __file_path) {
 			}
 		}
 		else if (_is_opening_tag_parse && std::regex_match(_line, _matches, _key_value_regex)) {
-			std::ssub_match _key = _matches[1];
-			std::ssub_match _value = _matches[2];
-			std::cout << _matches[2]  << std::endl;
-			if (removeQuote(_key) == "author")
-				_output.author = _value;
-			else if (removeQuote(_key) == "layout")
-				_output.layout = _value;
-			else if (removeQuote(_key) == "date")
-				_output.date = _value;
-			else if (removeQuote(_key) == "title")
-				_output.title = _value;
-			else if (removeQuote(_key) == "categories")
-				_output.categories = arr_extract(_value);
-			else if (removeQuote(_key) == "tags")
-				_output.tags = arr_extract(_value);
-			else
-				_output.others.insert(std::make_pair(_key.str(), _value.str()));
+			_key = _matches[1];
+			_value = _matches[2];
+			_output.list.insert(std::make_pair(_key.str(), _value.str()));
 		}
 	}
 
@@ -65,25 +69,23 @@ frontmatter extract_frontmatter(std::string __file_path) {
 
 int post_write(std::string __file_path, frontmatter __frontmatter, std::string __frontmatter_type = "YAML") {
 	std::ofstream __output_file;
+	std::regex _file_ext_md(".+\\.md$");
 
-	 frontmatter_type __f = init(__frontmatter_type);
-	
-	__output_file.open(__file_path);
+	if (!std::regex_match(__file_path, _file_ext_md))
+		__file_path += ".md";
 
-	if (!(__output_file.is_open()))
-		return return_error_code(3, "Can't open file at \"" + __file_path + "\".");
+	frontmatter_type __f = init(__frontmatter_type);
+	if (open_file_o(__output_file, __file_path) == FILE_IO_ERROR_NUM)
+		return FILE_IO_ERROR_NUM;
 
 	// Writing the frontmatter in the file
 	__output_file << __f.__open_divider << std::endl;
-	__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("layout"), "layout") << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(__frontmatter.layout), __frontmatter.layout) << is_json(__frontmatter_type, ",", "") << std::endl;
-	__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("title"), "title") << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(__frontmatter.title), __frontmatter.title) << is_json(__frontmatter_type, ",", "") << std::endl;
-	__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("date"), "date") << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(__frontmatter.date), __frontmatter.date) << is_json(__frontmatter_type, ",", "") << std::endl;
-	__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("author"), "author") << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(__frontmatter.author), __frontmatter.author) << is_json(__frontmatter_type, ",", "") << std::endl;
+	std::map<std::string, std::string>::iterator _trav = __frontmatter.list.begin();
 
-	if (__frontmatter.categories_length > 0)
-		__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("categories"), "categories") << __f.__assigner << " " << encloseQuote_arr(__frontmatter.categories, __frontmatter.categories_length) << is_json(__frontmatter_type, ",", "") << std::endl;
-	if (__frontmatter.tags_length > 0)
-		__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote("tags"), "tags") << __f.__assigner << " " << encloseQuote_arr(__frontmatter.tags, __frontmatter.tags_length) << std::endl;
+	while (_trav != __frontmatter.list.end()) {
+		__output_file << __f.__tab << is_json(__frontmatter_type, encloseQuote(_trav->first), _trav->first) << __f.__assigner << " " << is_json(__frontmatter_type, encloseQuote(_trav->second), _trav->second) << is_json(__frontmatter_type, ",", "") << std::endl;
+		_trav++;
+	}
 
 	__output_file << __f.__close_divider << std::endl;
 
@@ -97,10 +99,13 @@ int post_parse(std::string __file_path) {
 	return 0;
 }
 
-int post_extract(std::string __file_path, std::string __part = "frontmatter") {
+int post_extract(std::string __file_path, std::string __output_path, std::string __part = "frontmatter") {
 	frontmatter _fm = extract_frontmatter(__file_path);
 
-	std::cout << _fm.date << std::endl;
-
+	std::ofstream _output_file;
+	if (open_file_o(_output_file, __output_path) == FILE_IO_ERROR_NUM)
+		return FILE_IO_ERROR_NUM;
+	
+	_output_file.close();
 	return 0;
 }
